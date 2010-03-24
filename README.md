d712c1d1a5d5c3497a0541f29ae9ce10153d024e
Gilmore Davidson <gilmoreorless@gmail.com>
2010-03-24 22:21:03 +1100

    Added readme and minified version

---
+++ README.md
@@ -0,0 +1,26 @@
+jQuery IE6 CSS :hover support
+=============================
+
+This plugin scans through all CSS stylesheets and finds :hover pseudo-selectors, then adds the appropriate event handlers to make the CSS work in Internet Explorer 6.
+
+The function takes an optional boolean parameter, which if true will use `live()` instead of `bind()` to make sure all future dynamically-generated elements on a page are supported by the fix.
+
+There are smarts built in to not run if the browser is not IE6, and also not to run on `a:hover` selectors (since IE6 already supports `:hover` for `a` elements).
+
+Usage
+-----
+
+`$.ie6hover();` - Add hover support to only the elements on a page at the time the function is called.
+`$.ie6hover(true);` - Add hover support to all current and future elements on a page.
+
+Notes
+-----
+
+* This script will not work on stylesheets loaded from a domain different from the domain the page is on, due to cross-site security policies.
+* The hover scripts use the `mouseenter` and `mouseleave` events, unless you're using a jQuery version less than 1.4 in combination with the `live()` version of this function, in which case `mouseover` and `mouseout` are used (jQuery < 1.4 doesn't handle mouseenter and mouseleave in live events).
+* To save downloading the script for non-IE6 users, it's probably better to load the script within conditional comments:
+
+ <!--[if IE 6]>
+ <script type="text/javascript" src="jquery.ie6hover.js"></script>
+ <![endif]-->
+
--- jquery.ie6hover.js
+++ jquery.ie6hover.js
@@ -1,8 +1,8 @@
 /*!
- * jQuery IE6 hover support plug-in v0.1
+ * jQuery IE6 hover support plug-in v1.0
  * Add support for the :hover CSS pseudo-selector to IE6
  *
- * @requires jQuery v1.3.2 or later
+ * @requires jQuery v1.3 or later
  *
  * Copyright (c) 2010 Gilmore Davidson
  *
@@ -18,9 +18,13 @@
 			}
 			var klass = 'hover-ie6',
 				func = future === true ? 'live' : 'bind',
+				// jQuery < 1.4 can't handle 'mouseenter' and 'mouseleave' in live events
+				is14 = /^1.[4-9]/.test($.fn.jquery),
+				overEvent = is14 || !future ? 'mouseenter' : 'mouseover',
+				outEvent = is14 || !future ? 'mouseleave' : 'mouseout',
 				sheets = document.styleSheets,
-				check = /:hover\b/,
-				ignore = /\ba:hover\b/i,
+				check = /:hover\b/g,
+				ignore = /\ba:hover\b/ig,
 				selectors = [];
 			if (!sheets.length) {
 				return;
@@ -28,6 +32,7 @@
 			for (var i = 0, slen = sheets.length; i < slen; i++) {
 				var sheet = sheets[i],
 					rules;
+				// Gracefully handle any cross-domain security errors
 				try {
 					rules = sheet.rules;	
 				} catch (e) {
@@ -37,20 +42,25 @@
 					var rule = rules[j],
 						text = rule.selectorText;
 					if (check.test(text) && !ignore.test(text)) {
+						// Add the selector in a way that jQuery can handle (ie. no ":hover")
 						selectors.push(text.replace(check, ''));
+						// Replace ":hover" with ".hover-ie6" and add a new CSS rule
 						text = text.replace(check, '.' + klass);
+						// New CSS rule should be added at the same place as the existing rule to keep inheritance working
 						sheet.addRule(text, rule.style.cssText, j);
+						// Increase the counters due to the new rule being inserted
 						j++;
 						len++;
 					}
 				}
 			}
 			
+			// Add hover event handlers to selectors
 			if (selectors.length) {
 				$(function () {
-					$(selectors.join(','))[func]('mouseenter', function () {
+					$(selectors.join(','))[func](overEvent, function () {
 						$(this).addClass(klass);
-					})[func]('mouseleave', function () {
+					})[func](outEvent, function () {
 						$(this).removeClass(klass);
 					});
 				});
